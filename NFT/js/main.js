c01b9f651366a27828ab04cd2fbbd48a5c5db9b7
Yaroslav Korniienko <yarik.korn@yahoo.com>
2022-06-13 09:28:12 +0300

    added part of header

---
+++ NFT/js/main.js
@@ -0,0 +1,11 @@
+// const swiper = new Swiper('.swiper', {
+//   loop: true,
+//   speed: 400,
+//   slidesPerView: 1,
+//   effect: 'fade',
+//   autoplay: {
+//     delay: 3000,
+//   },
+
+// });
+new Swiper('.swiper');
