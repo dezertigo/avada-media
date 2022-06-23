05df67b37d1b5ca5d706833b516fd17e554e7e61
Yarik Korn <74897387+dezertigo@users.noreply.github.com>
2022-06-23 15:40:53 +0300

    updated files

--- NFT/js/main.js
+++ NFT/js/main.js
@@ -1,8 +1,25 @@
 const btnToggle = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu__list');
 const userNav = document.querySelector('.user-nav');
+const accordion = document.querySelectorAll('.footer-img');
+const exploreBtn = document.querySelector('.sellers-btn');
+const sellers = document.querySelector('.sellers__inner');
 
 btnToggle.addEventListener('click', () => {
- menu.classList.toggle('menu--active');
- userNav.classList.toggle('menu--active');
+    menu.classList.toggle('menu--active');
+    userNav.classList.toggle('menu--active');
 });
+
+exploreBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    sellers.classList.toggle('active');
+});
+
+accordion.forEach(item => (item.addEventListener('click', () => {
+    if (item.getAttribute('src')=='img/accordion-icon-reverse.svg') {
+        item.setAttribute('src', 'img/accordion-icon.svg');
+    } else {
+        item.setAttribute('src', 'img/accordion-icon-reverse.svg');
+    }
+    item.parentNode.parentNode.classList.toggle('active');
+})));
